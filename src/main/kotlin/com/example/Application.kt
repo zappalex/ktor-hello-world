e5476db2cb687965d298fffc8e6a2212d9d654ff
Alex Ashworth <aashworth@bignerdranch.com>
2023-11-04 12:08:55 -0400

    Initial commit of working ktor hello world with multiple modules

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,62 @@
+package com.example
+
+import com.example.plugins.*
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+// Server Engines Supported by Ktor: Netty, Jetty, Tomcat, CIO
+// running on localhost:8080/
+
+// Engine Main Approach  ================================================================
+
+//fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+//
+//// This will run on localhost:8080/
+//fun Application.module() {
+//    routing {
+//        get ("/module") {
+//            call.respondText ("Hello World!")
+//        }
+//    }
+//}
+//
+//fun Application.module_alt() {
+//    routing {
+//        get ("/module_alt") {
+//            call.respondText ("Hello Module Alt!")
+//        }
+//    }
+//}
+
+// Embedded Server Approach ================================================================
+
+fun main() {
+    embeddedServer(Netty, port = 8080) {
+        module()
+        module_alt()
+    }.start(wait = true)
+}
+
+/*
+    We can create multiple modules which extend from the Application Class. They can be grouped together, or can be
+    split into different classes.  The size and complexity of the server will dictate the need for multiple modules.
+ */
+
+fun Application.module() {
+    routing {
+        get ("/module") {
+            call.respondText ("Hello Module!")
+        }
+    }
+}
+
+fun Application.module_alt() {
+    routing {
+        get ("/module_alt") {
+            call.respondText ("Hello Module Alt!")
+        }
+    }
+}
