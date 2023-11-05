76c88780b16c7fc8977fe969d00b9f5555286c4a
Alex Ashworth <aashworth@bignerdranch.com>
2023-11-05 11:53:43 -0500

    removing unused libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,14 +26,9 @@ dependencies {
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-cbor:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-protobuf:$ktor_version")
-    implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
-
-    // serialization set up using -> https://github.com/ktorio/ktor-documentation/blob/2.3.5/codeSnippets/snippets/json-kotlinx/build.gradle.kts
 }
